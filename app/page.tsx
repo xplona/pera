e38aa34d10cef1528f5cdfa97bafca6736fe43e4
Osman Başar <osmanbasar@Osman-MacBook-Pro.local>
2026-01-12 01:15:13 +0300

    osman

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useMemo, useEffect } from 'react';
+import React, { useState, useMemo, useEffect, useRef } from 'react';
 import { 
   LayoutDashboard, 
   Package, 
